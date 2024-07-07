3a8031b5f715e78aac174db461fe810a450c3876
Kanishka Chandra <kanishkachandra4@gmail.com>
2024-07-07 15:50:44 +0530

    Section margin fixed

--- src/Sections/Home/Clients.jsx
+++ src/Sections/Home/Clients.jsx
@@ -3,7 +3,7 @@ import { CLIENT_LOGOS } from "../../data";
 
 const Clients = () => {
   return (
-    <div className="h-full w-full">
+    <div className="mb-6 h-full w-full">
       <h2 className="my-12 text-center text-5xl md:text-7xl">They Trust Us</h2>
       <div className="grid h-full place-content-start place-items-center px-10">
         <div className="grid h-full grid-cols-4 gap-4 md:grid-cols-6">
--- src/Sections/Home/Works.jsx
+++ src/Sections/Home/Works.jsx
@@ -4,7 +4,7 @@ import { PROJECT_IMAGES } from "../../data";
 
 const Works = () => {
   return (
-    <div className="mb-4 h-full w-full">
+    <div className="mb-6 h-full w-full">
       <h2 className="my-12 text-center text-5xl md:text-7xl">We Create</h2>
       <div className="grid h-full place-items-center px-10">
         <div className="grid h-full grid-cols-1 gap-4 md:grid-cols-3">
