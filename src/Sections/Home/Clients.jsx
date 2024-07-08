155c97eb9370cfe304bb4a77d910f83be836f95f
Kanishka Chandra <kanishkachandra4@gmail.com>
2024-07-08 20:05:08 +0530

    Alignment fixed

--- src/Sections/Home/Clients.jsx
+++ src/Sections/Home/Clients.jsx
@@ -3,8 +3,8 @@ import { CLIENT_LOGOS } from "../../data";
 
 const Clients = () => {
   return (
-    <div className="mb-6 h-full w-full">
-      <h2 className="my-12 text-center text-5xl md:text-7xl">They Trust Us</h2>
+    <div className="mb-28 mt-12 h-full w-full">
+      <h2 className="text-center text-5xl md:text-7xl">They Trust Us</h2>
       <div className="grid h-full place-content-start place-items-center px-10">
         <div className="grid h-full grid-cols-4 gap-4 md:grid-cols-6">
           {CLIENT_LOGOS.map((image, index) => (
@@ -16,7 +16,7 @@ const Clients = () => {
             />
           ))}
           <div className="flex h-full w-full items-center justify-center">
-            <h5 className="text-xl font-semibold md:text-2xl lg:text-3xl">
+            <h5 className="text-base font-semibold md:text-lg lg:text-xl">
               And many more...
             </h5>
           </div>
--- src/Sections/Home/Works.jsx
+++ src/Sections/Home/Works.jsx
@@ -4,8 +4,8 @@ import { PROJECT_IMAGES } from "../../data";
 
 const Works = () => {
   return (
-    <div className="mb-6 h-full w-full">
-      <h2 className="my-12 text-center text-5xl md:text-7xl">We Create</h2>
+    <div className="mb-12 h-full w-full">
+      <h2 className="my-12 text-center text-5xl md:text-7xl">Highlights</h2>
       <div className="grid h-full place-items-center px-10">
         <div className="grid h-full grid-cols-1 gap-4 md:grid-cols-3">
           {PROJECT_IMAGES.map((image, index) => (
